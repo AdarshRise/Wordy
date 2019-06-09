673d14b7a68740a7ed74beb67df571f3b6440c2f
adarshrise <adarshrise20@gmail.com>
2019-06-09 17:26:03 +0530

    Add project files.

---
+++ WordyWPF/UserControlShow2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WordyWPF
+{
+    /// <summary>
+    /// Interaction logic for UserControlShow2.xaml
+    /// </summary>
+    public partial class UserControlShow2 : UserControl
+    {
+        public UserControlShow2()
+        {
+            InitializeComponent();
+        }
+    }
+}
